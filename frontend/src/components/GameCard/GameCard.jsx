5701586bc47cee565cfee6ff1fbaa6cb8786d150
frenklinmici12 <frenklinmici12@gmail.com>
2026-01-06 00:18:59 -0600

    misc changes

--- frontend/src/components/GameCard/GameCard.jsx
+++ frontend/src/components/GameCard/GameCard.jsx
@@ -1,10 +1,12 @@
 import { Link } from 'react-router-dom';
 import './GameCard.css'; 
 
+import noImgAvail from './no-image-available.jpg';
+
 function GameCard({ game }) {
   return (
     <Link to={`/game/${game.id}`} className='game-card'>
-        <img src={game.background_image} alt={game.name}/>
+        <img src={game.background_image || noImgAvail} alt={game.name}/>
         <p>{game.name}</p>
     </Link>
   );
